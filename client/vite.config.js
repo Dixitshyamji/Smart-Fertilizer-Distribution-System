bf76c43d871448f48ddfd94316faf3f536d230e9
Shyam Dixit <shyam@example.com>
2026-08-23 17:47:37 +0530

    Allow render host in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,22 @@
+
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
 
-// // https://vite.dev/config/
+// // https://vitejs.dev/config/
 // export default defineConfig({
-//   plugins: [react()],
+//   plugins: [react(), tailwindcss()],
 // })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'smart-fertilizer-distribution-system-1.onrender.com',
+      '.onrender.com' // saare render subdomains allow karne ke liye
+    ]
+  }
 })
